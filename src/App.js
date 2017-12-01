9241289b160efaa6407ba7ff4c14a4daf59360d9
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-11-30 23:40:40 -0300

    Adiciona navbar teste e votação teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
 import './App.css';
-import Navbar from './components/navbar.ts';
+import Navbar from './components/navbar/navbar.js'
+import Votacao from './components/votacao/votacao.js'
 
 class App extends Component {
   render() {
@@ -10,13 +11,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Quem Me Representa?</h1>
-          <Navbar/>
+          <Navbar />
         </header>
         <p className="App-intro">
           Saiba quais deputados se parecem com você de acordo com as votações da câmara.
           Dê sua opinião nos temas listados e os deputados serão ordenados pela semelhança com a sua opinião dada.
           Você pode também selecionar seu estado ou filtrar pelo nome.
         </p>
+        <div className="App-intro">
+          <Votacao nome_votacao="Prosseguimento da denúncia contra o Temer?"/>
+        </div>
       </div>
     );
   }
