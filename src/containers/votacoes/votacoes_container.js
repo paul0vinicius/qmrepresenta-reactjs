c0182aea45f870e8e51c0eeaab207abb2fc60f67
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-12-12 17:18:20 -0300

    Inicializa as votações em 0 e escreve callback para notificar o main_container

--- src/containers/votacoes/votacoes_container.js
+++ src/containers/votacoes/votacoes_container.js
@@ -3,14 +3,15 @@ import Votacao from '../../components/votacao/votacao.js';
 import nomesVotacoes from '../../data/nomes_votacoes.json';
 
 class VotacoesContainer extends Component {
-
   constructor(props){
     super(props);
-    this.state = { scoreTotal: 0, votacoes: [], idVotacao: 0 };
+    var votacoesInicializadas = this.inicializaDicionarioVotacoes();
+    this.state = { votacoes: votacoesInicializadas };
   }
 
   render() {
       var votacoes = [];
+      // Acho que dá pra substituir por um map
       for(var i = 0; i < nomesVotacoes.length; i++){
         votacoes.push(<Votacao key = {nomesVotacoes[i].id_votacao}
                                idVotacao = {nomesVotacoes[i].id_votacao}
@@ -18,19 +19,32 @@ class VotacoesContainer extends Component {
                                callbackParent = { (newState) => this.onChildChange(newState) }
                                 />);
       }
-      return <div className="VotacoesContainer">{votacoes}Id:{this.state.idVotacao}/Value:{this.state.scoreTotal}</div>;
+      return <div className="VotacoesContainer">{votacoes}</div>;
+  }
+
+  inicializaDicionarioVotacoes(){
+    var votacoes = {};
+    for(var i = 0; i < nomesVotacoes.length; i++){
+      votacoes[nomesVotacoes[i].id_votacao] = 0;
+    }
+
+    return votacoes;
   }
 
 // Mapear todas as votações pelo ID no estilo: <idVotacao>|<valorVoto>
+// Essa função deve se chamar algo como PessoaVotou ou votacaoOcorreu
   onChildChange(newState){
-    // Atualizar as votações com um for e setar o novo estado com o que fora passado.
-    console.log(newState);
+    // Recupera o dicionário de votações ainda não atualizado
+    var votacoesNewState = this.state.votacoes;
+    // Atualiza dicionário
+    votacoesNewState[newState.idVotacao] = newState.value;
+    console.log(votacoesNewState);
     this.setState({
-      scoreTotal: newState.score,
-      idVotacao: newState.idVotacao
-    })
+      votacoes: votacoesNewState
+    });
+    // Avisa ao componente principal (main_container) que a pessoa votou em um novo tópico.
+    this.props.onVotacoesChange(votacoesNewState);
   }
-
 }
 
 export default VotacoesContainer;
