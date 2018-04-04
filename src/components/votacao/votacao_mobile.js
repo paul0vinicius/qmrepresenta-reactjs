499e0e8cfe120664207b7fd455293e3d1375905b
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-04 18:01:49 -0300

    Ajusta cor das votações (não finalizado)

--- src/components/votacao/votacao_mobile.js
+++ src/components/votacao/votacao_mobile.js
@@ -9,22 +9,22 @@ const divStyle = {
   //widht: '10vh'
 };
 
-const h3QuestionStyle = {
+const divQuestionStyle = {
   //height: '40vh',
   //widht: '10vh'
   //backgroundColor: "gray"
 };
 
-const h3YesStyle = {
+const divYesStyle = {
   //height: '40vh',
   //widht: '10vh'
-  backgroundColor: "green"
+  backgroundColor: "rgba(152,251,152,0.4)"
 };
 
-const h3NoStyle = {
+const divNoStyle = {
   //height: '40vh',
   //widht: '10vh'
-  backgroundColor: "red"
+  backgroundColor: "rgba(250,128,114,0.4)"
 };
 
 class VotacaoMobile extends Votacao {
@@ -46,9 +46,9 @@ class VotacaoMobile extends Votacao {
     return(
       <div style={divStyle}>
         <Slider {...settings}>
-          <div><h3 style={h3NoStyle}>{this.props.pergunta}</h3></div>
-          <div><h3 style={h3QuestionStyle}>{this.props.pergunta}</h3></div>
-          <div><h3 style={h3YesStyle}>{this.props.pergunta}</h3></div>
+          <div style={divNoStyle}><h3>{this.props.pergunta}</h3></div>
+          <div style={divQuestionStyle}><h3>{this.props.pergunta}</h3></div>
+          <div style={divYesStyle}><h3>{this.props.pergunta}</h3></div>
         </Slider>
       </div>
     );
