3fe9699c4a720763919cd2e9043250d58686570a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-04 17:42:41 -0300

    Torna swipe mais interessante

--- src/components/votacao/votacao_mobile.js
+++ src/components/votacao/votacao_mobile.js
@@ -9,25 +9,46 @@ const divStyle = {
   //widht: '10vh'
 };
 
+const h3QuestionStyle = {
+  //height: '40vh',
+  //widht: '10vh'
+  //backgroundColor: "gray"
+};
+
+const h3YesStyle = {
+  //height: '40vh',
+  //widht: '10vh'
+  backgroundColor: "green"
+};
+
+const h3NoStyle = {
+  //height: '40vh',
+  //widht: '10vh'
+  backgroundColor: "red"
+};
+
 class VotacaoMobile extends Votacao {
 
   render(){
 
     var settings = {
           infinite: false,
-          speed: 500,
+          speed: 800,
           slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 1,
+          useCSS: true,
+          useTransform: true,
+          fade: true,
           beforeChange: (oldIndex, newIndex) => this.onChildChange(newIndex)
         };
 
     return(
       <div style={divStyle}>
         <Slider {...settings}>
-          <div><h3>NAO</h3></div>
-          <div><h3>{this.props.pergunta}</h3></div>
-          <div><h3>SIM</h3></div>
+          <div><h3 style={h3NoStyle}>{this.props.pergunta}</h3></div>
+          <div><h3 style={h3QuestionStyle}>{this.props.pergunta}</h3></div>
+          <div><h3 style={h3YesStyle}>{this.props.pergunta}</h3></div>
         </Slider>
       </div>
     );
