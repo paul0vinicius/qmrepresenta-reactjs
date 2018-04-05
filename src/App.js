b5f10be555af9bf0c1d2bb77e5ce2db6e2f766a2
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-05 10:57:34 -0300

    Deixa view desktop como default

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 //import logo from './images/logo.svg';
 import './App.css';
 //import Navbar from './components/navbar/navbar.js';
-//import MainContainer from './containers/main/main_container.js';
+import MainContainer from './containers/main/main_container.js';
 import MainMobileContainer from './containers/main/main_mobile_container.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainMobileContainer />
+        <MainContainer />
       </div>
     );
   }
