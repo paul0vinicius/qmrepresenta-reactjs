2a0362f9a5bc2d0da10d93f3ed8f062081a595ff
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-13 15:22:05 -0300

    Adiciona novo modelo de gaveta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 //import logo from './images/logo.svg';
 import './App.css';
-//import Navbar from './components/navbar/navbar.js';
+import ButtonAppBar from './containers/app_bar/app_bar.js';
 import MainContainer from './containers/main/main_container.js';
+import Main from './containers/main/main.js';
 import MainMobileContainer from './containers/main/main_mobile_container.js';
+import MiniDrawer from './containers/menu_lateral/menu_lateral.js';
 
 class App extends Component {
   render() {
+    var mainC = <Main />;
     return (
       <div className="App">
-        <MainContainer />
+        <MiniDrawer main={mainC}/>
       </div>
     );
   }
