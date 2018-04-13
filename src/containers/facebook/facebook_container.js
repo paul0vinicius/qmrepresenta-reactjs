e113c083648fb9ae4e4aa37e1935c243f2707799
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-13 20:14:18 -0300

    Cria container para informações do facebook

---
+++ src/containers/facebook/facebook_container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+import PropTypes from 'prop-types';
+
+class FacebookContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Espaço reservado para o campo de Cálculo da compatibilidade.</div>
+    );
+  }
+}
+
+export default FacebookContainer;
