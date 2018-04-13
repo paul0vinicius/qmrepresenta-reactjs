572f3772a28365d59d15b33e17fc12858ee564e6
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-13 20:12:24 -0300

    Ajusta detalhes de layout no container principal

--- src/containers/main/main_container.js
+++ src/containers/main/main_container.js
@@ -6,12 +6,12 @@ import MiniDrawer from '../../containers/menu_lateral/menu_lateral.js';
 
 const votacoesGridStyle = {
   textAlign: 'left',
-  marginLeft: '3vh'
+  marginLeft: '0vh'
 }
 
 const deputadosGridStyle = {
   textAlign: 'left',
-  marginLeft: '3vh',
+  marginLeft: '0vh',
 }
 
 class MainContainer extends Component {
@@ -24,13 +24,11 @@ class MainContainer extends Component {
   render() {
     return(
       <div className="MainContainer">
-        <Grid container spacing={0}>
-          <Grid item xs={12} sm md lg style={votacoesGridStyle}>
-            <h3>Você é a favor de ...</h3>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={8} md={8} lg={8} style={votacoesGridStyle}>
             <VotacoesContainer onVotacoesChange = { (newState) => this.calculaCompatibilidade(newState) } />
           </Grid>
-          <Grid item xs={12} sm md lg style={deputadosGridStyle}>
-            <h3>Deputados</h3>
+          <Grid item xs={12} sm={4} md={4} lg={4} style={deputadosGridStyle}>
             <DeputadosContainer pegaVotacoesDeputados = { (votacoes) => this.setVotacoesDeputados(votacoes) }
                                 scoreDeputados = {this.state.scoreDeputados}
                                 />
