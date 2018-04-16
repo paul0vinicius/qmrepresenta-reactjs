7293a3d869420ee42dc86ae83d5aecb55d338c71
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-16 14:28:28 -0300

    Insere detecção de mudança de tela

--- src/App.js
+++ src/App.js
@@ -8,13 +8,37 @@ import MainMobileContainer from './containers/main/main_mobile_container.js';
 import MiniDrawer from './containers/menu_lateral/menu_lateral.js';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
   render() {
-    var mainC = <Main />;
+    const { width } = this.state;
+    const isMobile = width <= 500;
+
+    var mainC = isMobile ? <MainMobileContainer /> : <Main />;
+
     return (
       <div className="App">
         <MiniDrawer main={mainC}/>
       </div>
-    );
+      );
   }
 }
 
