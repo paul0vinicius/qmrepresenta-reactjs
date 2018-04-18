eceaeb1a61f0238a471c15d1b12153a5c0f49c1a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-04-18 15:51:37 -0300

    Ajusta card de deputado

--- src/views/mobile-view/components/deputado/DeputadoMobile.js
+++ src/views/mobile-view/components/deputado/DeputadoMobile.js
@@ -1,49 +1,42 @@
 import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
 import PropTypes from 'prop-types';
-import { CircularProgress } from 'material-ui/Progress';
+import { LinearProgress } from 'material-ui/Progress';
 import Grid from 'material-ui/Grid';
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
-
-const avatarStyle = {
-  contentAlign: 'center',
-  bigAvatar:{
-    width: 60,
-    height: 60,
-  },
-}
+import ImageAvatar from './ImageAvatar.js';
+import NomeEPartido from './NomeEPartido.js';
 
 const containerStyle = {
-  marginRight: '1vh',
-  marginLeft: '1vh'
+  marginRight: '0vh',
+  marginLeft: '2vh'
+}
+
+const progressBarStyle = {
+  marginTop: '2vh',
+  marginRight: '0vh'
 }
 
 class DeputadoMobile extends Component {
 
   render(){
 
-    var progress = <CircularProgress variant="determinate" value={this.props.score*100} />;
+    var progress = <LinearProgress variant="determinate" value={this.props.score*100} />;
 
     return(
       <div className="DeputadoMobile">
-        <Grid container style={containerStyle}>
-          <Grid item xs={12}>
-            <Avatar alt="" src={this.props.foto} />
+        <Grid container>
+          <Grid item xs={4}>
+            <ImageAvatar src={this.props.foto} alt=""/>
+          </Grid>
+          <Grid item xs={4} style={progressBarStyle}>
+            {progress}
+            {this.props.score}
           </Grid>
           <Grid item xs={4}>
-            {this.props.nome}
+
           </Grid>
-          <Grid item xs={12}>
-            {this.props.partido}/{this.props.uf}
-          </Grid>
-          <Grid item xs={12}>
-            <div className={styles.root}>
-            </div>
+          <Grid item xs={2} style={containerStyle}>
+            <NomeEPartido nome={this.props.nome} partido={this.props.partido} uf={this.props.uf}/>
           </Grid>
         </Grid>
       </div>
