c68bb0b3ff9738cab8041da1fdb0efbe4179a9d2
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-05-07 15:51:08 -0300

    Adiciona filtros por nome/partido/estado
    Estão funcionando separadamente, não sei por quê.

--- src/views/mobile-view/containers/deputados/DeputadosMobileContainer.js
+++ src/views/mobile-view/containers/deputados/DeputadosMobileContainer.js
@@ -5,6 +5,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import DeputadoFactory from '../../../../factories/DeputadoFactory.js';
 import DeputadosContainer from '../../../desktop-view/containers/deputados/DeputadosContainer.js';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+import TextField from 'material-ui/TextField';
 
 const cardStyle = {
   margin: '0vh',
@@ -17,10 +22,10 @@ class DeputadosMobileContainer extends DeputadosContainer {
 
   constructor(props){
     super(props);
-    this.state = { deputados: [], filterName: '', filterUf: '' };
+    this.state = { deputados: [], filterName: '', filterUf: '', filterPartido: '' };
   }
 
-  componentDidMount(){
+  componentWillMount(){
     let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
     this.setState({deputados: deputados});
   }
@@ -35,13 +40,35 @@ class DeputadosMobileContainer extends DeputadosContainer {
       //swipeToSlide: true,
         };
 
-    //console.log(deputados)
-    console.log(this.deputadoE);
-
     return(
       <div className="DeputadosMobileContainer">
-        <input type="text" placeholder="Nome do deputado" onChange={this.buscaNome.bind(this)}/>
-        <input type="text" placeholder="UF" onChange={this.buscaUF.bind(this)}/>
+      <TextField label="Nome do Deputado" onChange={this.buscaNome.bind(this)}/>
+        <FormControl>
+          <InputLabel htmlFor="uf-simple">UF</InputLabel>
+          <Select
+            value={this.state.filterUf}
+            onChange={this.buscaUF.bind(this)}
+            inputProps={{
+              name: 'uf',
+              id: 'uf-simple',
+            }}
+          >
+          {this.estados()}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="partido-simple">Partido</InputLabel>
+          <Select
+            value={this.state.filterPartido}
+            onChange={this.buscaPartido.bind(this)}
+            inputProps={{
+              name: 'partido',
+              id: 'partido-simple',
+            }}
+          >
+          {this.partidos()}
+          </Select>
+        </FormControl>
         <Card style={cardStyle}>
             {this.state.deputados.slice(0,20)}
         </Card>
@@ -53,8 +80,9 @@ class DeputadosMobileContainer extends DeputadosContainer {
     var nomeDeputado = evento.target.value;
     this.setState({filterName: nomeDeputado})
 
-    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
-    deputados = deputados.filter(d => (d.props.children.props.nome.toLowerCase().indexOf(this.state.filterName.toLowerCase()) >= 0));
+    console.log(nomeDeputado);
+    var deputados = this.filtraPorNome(nomeDeputado);
+
     this.setState({deputados: deputados})
   }
 
@@ -63,12 +91,57 @@ class DeputadosMobileContainer extends DeputadosContainer {
     this.setState({filterUf: UFDeputado})
 
     console.log(UFDeputado);
-    
-    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
-    deputados = deputados.filter(d => (d.props.children.props.uf.toLowerCase() === this.state.filterUf.toLowerCase()));
+    var deputados = this.filtraPorUf(UFDeputado);
+
     this.setState({deputados: deputados})
   }
 
+  buscaPartido(evento){
+    var partidoDeputado = evento.target.value;
+    this.setState({filterPartido: partidoDeputado})
+
+    console.log(partidoDeputado);
+    var deputados = this.filtraPorPartido(partidoDeputado);
+
+    this.setState({deputados: deputados})
+  }
+
+  filtraPorPartido(partidoDeputado){
+    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
+    return deputados.filter(d => ((d.props.children.props.partido.toLowerCase()) === partidoDeputado.toLowerCase()));
+  }
+
+  filtraPorUf(UFDeputado){
+    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
+    return deputados.filter(d => ((d.props.children.props.uf.toLowerCase()) === UFDeputado.toLowerCase()));
+    //this.setState({deputados: deputados})
+    //console.log(deputados);
+  }
+
+  filtraPorNome(nomeDeputado){
+    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
+    return deputados.filter(d => (d.props.children.props.nome.toLowerCase().indexOf(nomeDeputado.toLowerCase()) >= 0));
+    //this.setState({deputados: deputados})
+  }
+
+  estados(){
+    var estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA",
+                   "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+                   "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+
+    return estados.map(function(a){return <MenuItem key={a} value={a}>{a}</MenuItem>})
+  }
+
+  partidos(){
+    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", this.props.scoreDeputados, this.nVotacoesDep);
+    let partidos = new Set();
+
+    for(var i = 0; i < deputados.length; i++){
+      partidos.add(deputados[i].props.children.props.partido);
+    }
+
+    return Array.from(partidos).map(function(a){return <MenuItem key={a} value={a}>{a}</MenuItem>})
+  }
 }
 
 export default DeputadosMobileContainer;
