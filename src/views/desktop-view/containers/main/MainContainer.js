5d08dbaeb2f01df573fc34f3e33fdfb92404cfa0
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-05-24 18:03:24 -0300

    Insere containers de votação e deputados dentro do AppBar
    Dessa forma, ao alternar entre as seções, os novos componentes são renderizados.

--- src/views/desktop-view/containers/main/MainContainer.js
+++ src/views/desktop-view/containers/main/MainContainer.js
@@ -2,16 +2,52 @@ import React, { Component } from 'react';
 import VotacoesContainer from '../votacoes/VotacoesContainer.js';
 import DeputadosContainer from '../deputados/DeputadosContainer.js';
 import Grid from 'material-ui/Grid';
+import SimpleAppBar from './AppBar.js';
 import MiniDrawer from '../../containers/menu_lateral/SideBar.js';
+import NavigationBar from './NavigationBar.js';
+import grey from 'material-ui/colors/grey';
 
 const votacoesGridStyle = {
   textAlign: 'left',
-  marginLeft: '0vh'
+  marginLeft: '0vh',
+  //width: '10vh'
 }
 
 const deputadosGridStyle = {
   textAlign: 'left',
   marginLeft: '0vh',
+  //width: '10vh'
+}
+
+const mainGridStyle = {
+  height: '100vh',
+  overflowY: 'hidden',
+  overflowX: 'hidden',
+}
+
+const a = {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '50%',
+  backgroundColor: '#51269C',
+}
+
+const appBarStyle = {
+  marginTop: '4vh',
+  marginLeft: '25vh',
+  marginRight: '15vh',
+  marginBottom: '-7vh'
+}
+
+const b = {
+  position: 'fixed',
+  top: '50%',
+  left: '0',
+  width: '100%',
+  height: '50%',
+  backgroundColor: '#1D6B91',
 }
 
 class MainContainer extends Component {
@@ -22,16 +58,22 @@ class MainContainer extends Component {
   }
 
   render() {
+    var votacoesContainer = <VotacoesContainer onVotacoesChange = { (newState) => this.calculaCompatibilidade(newState) } />;
+    var deputadosContainer = <DeputadosContainer pegaVotacoesDeputados = { (votacoes) => this.setVotacoesDeputados(votacoes) }
+                        scoreDeputados = {this.state.scoreDeputados}
+                        />;
     return(
-      <div className="MainContainer">
+      <div className="MainContainer" style={mainGridStyle}>
         <Grid container spacing={24}>
-          <Grid item xs={12} sm={8} md={8} lg={8} style={votacoesGridStyle}>
-            <VotacoesContainer onVotacoesChange = { (newState) => this.calculaCompatibilidade(newState) } />
+          <div style={a}></div>
+          <div style={b}></div>
+          <Grid item xs={12} sm={12} md={12} lg={12} style={appBarStyle}>
+            <SimpleAppBar />
           </Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4} style={deputadosGridStyle}>
-            <DeputadosContainer pegaVotacoesDeputados = { (votacoes) => this.setVotacoesDeputados(votacoes) }
-                                scoreDeputados = {this.state.scoreDeputados}
-                                />
+          <Grid item xs={12} sm={12} md={12} lg={12} style={appBarStyle}>
+            <NavigationBar votacoes={votacoesContainer}
+                           deputados={deputadosContainer}
+                            />
           </Grid>
         </Grid>
       </div>
