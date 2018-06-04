34f5dbd607e361f72b56c6d8c9e2722e50cea7bc
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-04 17:59:02 -0300

    Ajusta detalhes para exibição de partidos

--- src/views/desktop-view/components/partido/Partido.js
+++ src/views/desktop-view/components/partido/Partido.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { LinearProgress } from 'material-ui/Progress';
 import Grid from 'material-ui/Grid';
+import Avatar from 'material-ui/Avatar';
 
 const DivProgress = (props) => {
   //console.log(props.score);
   if(props.score !== undefined){
     return(
-      <div style={divProgressStyle}>{(props.score*100).toFixed(2)}%</div>
+      <div>{(props.score*100).toFixed(2)}%</div>
     );
   } else return <div></div>;
 }
@@ -16,21 +17,26 @@ class Partido extends Component {
   render(){
 
     let linearProgress = <LinearProgress variant="determinate"
-                            style={progressBarStyle} size={80} value={this.props.score*100} />;
+                            size={80} value={this.props.score*100} />;
 
     let divProgressElement = <DivProgress score={this.props.score}/>
 
     return(
       <div className="Partido">
         <Grid container>
+          <Grid item xs sm md lg>
+            <Avatar alt="" src={this.props.foto} styles={{ width: 100, height: 100}}/>
+          </Grid>
           <Grid item xs={2}>
+            {this.props.nome}
           </Grid>
           <Grid item xs={3}>
+            {this.props.sigla}
           </Grid>
-          <Grid item xs={3} style={infoStyles}>
+          <Grid item xs={3}>
             {linearProgress}
           </Grid>
-          <Grid item xs={3} style={divProgressStyles}>
+          <Grid item xs={3}>
             {divProgressElement}
           </Grid>
         </Grid>
---
+++ src/views/desktop-view/containers/partidos/PartidosContainer.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+//import DeputadoFactory from '../../../../factories/DeputadoFactory.js';
+import PartidoFactory from '../../../../factories/PartidoFactory.js';
+//import infoDeputados from '../../../../data/deputados.json';
+import infoPartidos from '../../../../data/partidos.json';
+import Card from 'material-ui/Card';
+//import TabsContainer from '../TabsContainer.js';
+import nomesVotacoes from '../../../../data/nomes_votacoes.json';
+import FlipMove from 'react-flip-move';
+
+const cardStyle = {
+  //overflowY: 'scroll',
+  //overflowX: 'hidden',
+  //height: '90vh',
+  position: 'fixed',
+  //backgroundColor: "#DBDBDB",
+  //width:'2000vh',
+  //marginLeft: '7vh',
+  //marginRight: '15vh'
+};
+
+class PartidosContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.nVotacoesPartido = this.calculaNVotacoesPartidos(this.getVotacoesPartidos());
+    this.scorePartidos = {};
+    this.state = { partidos: [] };
+  }
+
+  componentWillReceiveProps(nextProps){
+    let partidos = PartidoFactory.inicializaComponentesPartidos("", nextProps.scorePartidos, this.nVotacoesPartido);
+    this.setState({partidos: partidos});
+  }
+
+  componentDidMount() {
+    this.props.pegaVotacoesPartidos(this.getVotacoesPartidos());
+    let partidos = PartidoFactory.inicializaComponentesPartidos("", this.props.scorePartidos, this.nVotacoesPartido);
+    this.setState({partidos: partidos});
+  }
+
+  render(){
+    return (
+      <div>
+        <FlipMove>
+          {this.state.partidos}
+        </FlipMove>
+      </div>
+    );
+  }
+
+  getVotacoesPartidos(){
+    var todasVotacoes = {};
+    for (var i = 0; i < infoPartidos.length; i++){
+      var votacoes = {};
+      for (var j = 0; j < Object.keys(infoPartidos[i].votacoes).length; j++){
+        votacoes[infoPartidos[i].votacoes[j].id_votacao] = infoPartidos[i].votacoes[j].value;
+      }
+      todasVotacoes[infoPartidos[i].id_partido] = votacoes;
+    }
+    return todasVotacoes;
+  }
+
+  calculaNVotacoesPartidos(votacoes){
+
+    console.log(votacoes);
+
+    var nVotacoesPartObject = {};
+    for (var partido in votacoes){
+      var nVotacoesPart = 0;
+      for(var i = 0; i < nomesVotacoes.length; i++){
+        if(votacoes[partido][nomesVotacoes[i].id_votacao] !== 0) nVotacoesPart++;
+      }
+      nVotacoesPartObject[partido] = nVotacoesPart;
+    }
+
+    return nVotacoesPartObject;
+  }
+
+}
+
+export default PartidosContainer;
