398c5e4608a56a75d6c7286c5fadf73c752e208a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-04 18:02:11 -0300

    Adiciona container de partidos e cria função de compatibilidade

--- src/views/desktop-view/containers/main/MainContainer.js
+++ src/views/desktop-view/containers/main/MainContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import VotacoesContainer from '../votacoes/VotacoesContainer.js';
 import DeputadosContainer from '../deputados/DeputadosContainer.js';
+import PartidosContainer from '../partidos/PartidosContainer.js';
 import Grid from 'material-ui/Grid';
 import SimpleAppBar from './AppBar.js';
 import MiniDrawer from '../../containers/menu_lateral/SideBar.js';
 import NavigationBar from './NavigationBar.js';
 import grey from 'material-ui/colors/grey';
+import TabsContainer from './TabsContainer.js';
 
 const votacoesGridStyle = {
   textAlign: 'left',
@@ -53,15 +55,23 @@ const b = {
 class MainContainer extends Component {
   constructor(props){
     super(props);
-    this.state = { scoreDeputados: {}};
-    this.todasVotacoes = {};
+    this.state = { scoreDeputados: {}, scorePartidos: {}};
+    this.todasVotacoesDeputados = {};
+    this.todasVotacoesPartidos = {};
   }
 
   render() {
     var votacoesContainer = <VotacoesContainer onVotacoesChange = { (newState) => this.calculaCompatibilidade(newState) } />;
+    // Renomear deputadosContainer para DeputadosEPartidosContainer... Algo assim
     var deputadosContainer = <DeputadosContainer pegaVotacoesDeputados = { (votacoes) => this.setVotacoesDeputados(votacoes) }
                         scoreDeputados = {this.state.scoreDeputados}
                         />;
+    var partidosContainer = <PartidosContainer pegaVotacoesPartidos = { (votacoes) => this.setVotacoesPartidos(votacoes) }
+                        scorePartidos = {this.state.scorePartidos}
+                        />;
+    var deputadosEPartidosContainer = <TabsContainer deputados={deputadosContainer}
+                        partidos={partidosContainer}
+    />;
     return(
       <div className="MainContainer" style={mainGridStyle}>
         <Grid container spacing={24}>
@@ -72,7 +82,7 @@ class MainContainer extends Component {
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12} style={appBarStyle}>
             <NavigationBar votacoes={votacoesContainer}
-                           deputados={deputadosContainer}
+                           deputadosEPartidos={deputadosEPartidosContainer}
                             />
           </Grid>
         </Grid>
@@ -87,19 +97,20 @@ class MainContainer extends Component {
   // Fórmula de compatibilidade: score(u,d) = votacoes_iguais(u,d)/total_votacoes(u,d)
   calculaCompatibilidade(newState){
     var newScoreDeputados = {};
-    var newIsPoucoVoto = {};
-    for (var deputado in this.todasVotacoes){
+    var newScorePartidos = {};
+    //var newIsPoucoVoto = {};
+    for (var deputado in this.todasVotacoesDeputados){
       var score = 0;
       var antiScore = 0;
       var ambosVotaram = 0;
       var nVotacoesDep = 0;
       var nVotacoesUser = 0;
       for (var idVotacao in newState){
-        if (newState[idVotacao] === this.todasVotacoes[deputado][idVotacao] &&
+        if (newState[idVotacao] === this.todasVotacoesDeputados[deputado][idVotacao] &&
             newState[idVotacao] !== 0) score++;
-        else if(newState[idVotacao] !== this.todasVotacoes[deputado][idVotacao] &&
+        else if(newState[idVotacao] !== this.todasVotacoesDeputados[deputado][idVotacao] &&
             newState[idVotacao] !== 0) antiScore++;
-        if ((newState[idVotacao] !== 0) && (this.todasVotacoes[deputado][idVotacao] !== 0)) ambosVotaram++;
+        if ((newState[idVotacao] !== 0) && (this.todasVotacoesDeputados[deputado][idVotacao] !== 0)) ambosVotaram++;
         if(newState[idVotacao] !== 0) nVotacoesUser++;
       }
       if (ambosVotaram === 0) ambosVotaram = 1;
@@ -108,13 +119,36 @@ class MainContainer extends Component {
       //console.log(antiScore);
     }
 
+    for (var partido in this.todasVotacoesPartidos) {
+      var score = 0;
+      var antiScore = 0;
+      var ambosVotaram = 0;
+      var nVotacoesPart = 0;
+      var nVotacoesUser = 0;
+      for (var idVotacao in newState){
+        if (newState[idVotacao] === this.todasVotacoesPartidos[partido][idVotacao] &&
+            newState[idVotacao] !== 0) score++;
+        else if(newState[idVotacao] !== this.todasVotacoesPartidos[partido][idVotacao] &&
+            newState[idVotacao] !== 0) antiScore++;
+        if ((newState[idVotacao] !== 0) && (this.todasVotacoesPartidos[partido][idVotacao] !== 0)) ambosVotaram++;
+        if(newState[idVotacao] !== 0) nVotacoesUser++;
+      }
+      if (ambosVotaram === 0) ambosVotaram = 1;
+      newScorePartidos[partido] = score/ambosVotaram;
+    }
+
     this.setState({
-      scoreDeputados: newScoreDeputados
+      scoreDeputados: newScoreDeputados,
+      scorePartidos: newScorePartidos
     })
   }
 
   setVotacoesDeputados(votacoes){
-    this.todasVotacoes = votacoes;
+    this.todasVotacoesDeputados = votacoes;
+  }
+
+  setVotacoesPartidos(votacoes){
+    this.todasVotacoesPartidos = votacoes;
   }
 
 }
