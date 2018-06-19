c18e791fc19698b05dd0055ac6747f61004dea7a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-19 08:54:01 -0300

    Cria containers para as seções de análises, contato e sobre

---
+++ src/views/mobile-view/containers/sobre/SobreContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+import PropTypes from 'prop-types';
+
+class SobreContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Espaço reservado para o campo Sobre.</div>
+    );
+  }
+}
+
+export default SobreContainer;
