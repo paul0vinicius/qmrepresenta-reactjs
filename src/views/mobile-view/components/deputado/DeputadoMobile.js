7e90bcb91fbe9292c7b1eba24b99f1612707e717
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-06-25 17:58:31 -0300

    Ajusta deputados para ant-design

--- src/views/mobile-view/components/deputado/DeputadoMobile.js
+++ src/views/mobile-view/components/deputado/DeputadoMobile.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Avatar from 'material-ui/Avatar';
 import PropTypes from 'prop-types';
 import { LinearProgress } from 'material-ui/Progress';
 import { CircularProgress } from 'material-ui/Progress';
 import Grid from 'material-ui/Grid';
 import ImageAvatar from '../../../common-components/ImageAvatar.js';
 import NomeEPartido from '../../../common-components/NomeEPartido.js';
+import { Row, Col } from 'antd';
+import { Avatar } from 'antd';
+import { Progress } from 'antd';
 
 const containerStyle = {
   //marginRight: '0vh',
@@ -60,24 +62,37 @@ class DeputadoMobile extends Component {
 
     return(
       <div className="DeputadoMobile">
-        <Grid container>
-          <Grid item xs={2}>
-          </Grid>
-          <Grid item xs={3}>
-            <ImageAvatar src={this.props.foto} alt="" />
-            {this.state.showProgressDiv && divProgressElement}
-          </Grid>
-          <Grid item xs={3} style={infoStyles}>
-            {linearProgress}
-            <NomeEPartido nome={this.props.nome} partido={this.props.partido} uf={this.props.uf}/>
-          </Grid>
-          <Grid item xs={3} style={divProgressStyles}>
-            {divProgressElement}
-          </Grid>
-        </Grid>
+        <Row>
+          <Col offset={7} span={3}>
+            <Avatar size="large" src={this.props.foto} />
+          </Col>
+          <Col span={6}><Progress percent={Math.round(this.props.score*100)} size="small"/> </Col>
+          <Col span={10}>{this.props.votosSimilares.length}/{this.props.nVotosUsuario}</Col>
+          <Col offset={10} span={12}>{this.props.nome}</Col>
+          <Col offset={10} span={12}>{this.props.partido}/{this.props.uf}</Col>
+        </Row>
       </div>
     );
   }
+
+
+  // a(){
+  //   <Grid container>
+  //   <Grid item xs={2}>
+  //   </Grid>
+  //   <Grid item xs={3}>
+  //     <ImageAvatar src={this.props.foto} alt="" />
+  //     {this.state.showProgressDiv && divProgressElement}
+  //   </Grid>
+  //   <Grid item xs={3} style={infoStyles}>
+  //     {linearProgress}
+  //     <NomeEPartido nome={this.props.nome} partido={this.props.partido} uf={this.props.uf}/>
+  //   </Grid>
+  //   <Grid item xs={3} style={divProgressStyles}>
+  //     {divProgressElement}
+  //   </Grid>
+  // </Grid>
+  // }
 
   showHideProgress(){
     //console.log("onClick");
