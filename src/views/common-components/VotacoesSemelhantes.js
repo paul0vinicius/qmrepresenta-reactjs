a488ca8aa1258087bf4fe3bb5c9670fef726a0f3
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-09 18:13:06 -0300

    Move VotacoesSemelhantes para a pasta de componentes comuns

---
+++ src/views/common-components/VotacoesSemelhantes.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Modal, Button } from "antd";
+
+class VotacoesSemelhantes extends Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.showModal}>{this.props.buttonName}</Button>
+        <Modal
+          title={this.props.modalTitle}
+          visible={this.state.visible}
+          style={{ top: 10 }}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[]}
+        >
+          {this.props.content}
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default VotacoesSemelhantes;
