308e0db7c6b9fe2e576c34abf3e3ab24a11aafdc
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-10 10:35:48 -0300

    Ajusta design de código com prettier e importa painel de votações inicial

---
+++ src/views/desktop-view/containers/deputados/PainelInicial.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class PainelInicial extends Component {
+  render() {
+    return (
+      <div>
+        <p>Vamos começar?</p>
+        <p>Vote ao lado e veja qual deputado mais se identifica com você. </p>
+        <p>
+          Você também pode usar os filtros acima para buscar o deputado do seu
+          estado ou do partido de sua preferência.
+        </p>
+      </div>
+    );
+  }
+}
