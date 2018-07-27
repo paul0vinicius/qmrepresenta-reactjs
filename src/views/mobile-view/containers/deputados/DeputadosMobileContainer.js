1a22f8a981485bf91c4923023217073430ab00fe
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-27 16:23:49 -0300

    Ajusta style

--- src/views/mobile-view/containers/deputados/DeputadosMobileContainer.js
+++ src/views/mobile-view/containers/deputados/DeputadosMobileContainer.js
@@ -1,46 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import Card from 'material-ui/Card';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import DeputadoFactory from '../../../../factories/DeputadoFactory.js';
-import DeputadosContainer from '../../../desktop-view/containers/deputados/DeputadosContainer.js';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import DeputadoFactory from "../../../../factories/DeputadoFactory.js";
+import DeputadosContainer from "../../../desktop-view/containers/deputados/DeputadosContainer.js";
 //import Input, { InputLabel } from 'material-ui/Input';
-import { MenuItem } from 'material-ui/Menu';
-import { FormControl, FormHelperText } from 'material-ui/Form';
+import { MenuItem } from "material-ui/Menu";
+import { FormControl, FormHelperText } from "material-ui/Form";
 //import Select from 'material-ui/Select';
-import TextField from 'material-ui/TextField';
-import FlipMove from 'react-flip-move';
-import { Card } from 'antd';
-import { painelInicial } from './PainelInicial.js';
-import { Input, Select, Icon } from 'antd';
-import { Row, Col } from 'antd';
+import TextField from "material-ui/TextField";
+import FlipMove from "react-flip-move";
+import { Card } from "antd";
+import { painelInicial } from "./PainelInicial.js";
+import { Input, Select, Icon } from "antd";
+import { Row, Col } from "antd";
 
 const Search = Input.Search;
 const Option = Select.Option;
 
 const cardStyle = {
-  margin: '0vh',
-  height: '35vh',
-  overflowY: 'scroll',
+  margin: "0vh",
+  height: "35vh",
+  overflowY: "scroll"
   //width: '60vh'
 };
 
 class DeputadosMobileContainer extends DeputadosContainer {
-  componentWillReceiveProps(nextProps){
-    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile", nextProps.scoreDeputados, this.nVotacoesDep, nextProps.votosSimilares, nextProps.votosUsuario);
-    this.setState({deputados: deputados});
+  componentWillReceiveProps(nextProps) {
+    let deputados = DeputadoFactory.inicializaComponentesDeputados(
+      "mobile",
+      nextProps.scoreDeputados,
+      this.nVotacoesDep,
+      nextProps.votosSimilares,
+      nextProps.votosUsuario
+    );
+    this.setState({ deputados: deputados });
   }
 
   componentDidMount() {
     //this.props.pegaVotacoesDeputados(this.getVotacoes());
     //this.props.pegaVotacoesPartidos(this.getVotacoesPartidos());
-    let deputados = DeputadoFactory.inicializaComponentesDeputados("mobile",this.props.scoreDeputados, this.nVotacoesDep, this.props.votosSimilares, this.props.votosUsuario);
+    let deputados = DeputadoFactory.inicializaComponentesDeputados(
+      "mobile",
+      this.props.scoreDeputados,
+      this.nVotacoesDep,
+      this.props.votosSimilares,
+      this.props.votosUsuario
+    );
     //let partidos = PartidoFactory.inicializaComponentesPartidos("", nextProps.scorePartidos, this.nVotacoesPartido);
-    this.setState({deputados: deputados});
+    this.setState({ deputados: deputados });
   }
 
-  render(){
+  render() {
     var settings = {
       className: "center",
       infinite: false,
@@ -48,32 +60,56 @@ class DeputadosMobileContainer extends DeputadosContainer {
       slidesToShow: 1,
       slidesToScroll: 1
       //swipeToSlide: true,
-        };
+    };
 
-    let byName = this.state.filterName !== '' ? this.state.deputados.filter(d => this.state.deputadosPorNome.indexOf(d.key) !== -1) : this.state.deputados;
-    let byUf = this.state.filterUf !== 'TODOS' ? byName.filter(d => this.state.deputadosPorUf.indexOf(d.key) !== -1) : byName;
-    var deputadosAExibir = this.state.filterPartido !== 'TODOS' ? byUf.filter(d => this.state.deputadosPorPartido.indexOf(d.key) !== -1) : byUf;
+    let byName =
+      this.state.filterName !== ""
+        ? this.state.deputados.filter(
+            d => this.state.deputadosPorNome.indexOf(d.key) !== -1
+          )
+        : this.state.deputados;
+    let byUf =
+      this.state.filterUf !== "TODOS"
+        ? byName.filter(d => this.state.deputadosPorUf.indexOf(d.key) !== -1)
+        : byName;
+    var deputadosAExibir =
+      this.state.filterPartido !== "TODOS"
+        ? byUf.filter(d => this.state.deputadosPorPartido.indexOf(d.key) !== -1)
+        : byUf;
 
-    return(
+    return (
       <div className="DeputadosMobileContainer">
         <Row>
           <Col span={12}>
-            <Search placeholder="Nome do deputado" onChange={this.buscaNome.bind(this)} />
+            <Search
+              placeholder="Nome do deputado"
+              onChange={this.buscaNome.bind(this)}
+            />
           </Col>
           <Col span={6}>
-              <Select style={{ width: 90}} placeholder="Partidos" onChange={this.buscaPartido.bind(this)}>
+            <Select
+              style={{ width: 90 }}
+              placeholder="Partidos"
+              onChange={this.buscaPartido.bind(this)}
+            >
               {this.partidos()}
-              </Select>
+            </Select>
           </Col>
           <Col span={6}>
-              <Select style={{ width: 90}} placeholder="Estados" onChange={this.buscaUF.bind(this)}>
+            <Select
+              style={{ width: 90 }}
+              placeholder="Estados"
+              onChange={this.buscaUF.bind(this)}
+            >
               {this.estados()}
-              </Select>
+            </Select>
           </Col>
           <Col span={24}>
             <Card style={cardStyle}>
               <FlipMove>
-                {deputadosAExibir.length !== 0 ? deputadosAExibir.slice(0,20) : painelInicial}
+                {deputadosAExibir.length !== 0
+                  ? deputadosAExibir.slice(0, 20)
+                  : painelInicial}
               </FlipMove>
             </Card>
           </Col>
--- src/views/mobile-view/containers/menu_lateral/MobileDrawer.js
+++ src/views/mobile-view/containers/menu_lateral/MobileDrawer.js
@@ -1,53 +1,56 @@
-import React, { Component } from 'react';
-import Drawer from 'rc-drawer';
-import { Menu, Icon } from 'antd';
-import { linksList } from './MenuSection';
-import 'antd/lib/style';
-import 'antd/lib/menu/style';
+import React, { Component } from "react";
+import Drawer from "rc-drawer";
+import { Menu, Icon } from "antd";
+import { linksList } from "./MenuSection";
+import "antd/lib/style";
+import "antd/lib/menu/style";
 
-import 'rc-drawer/assets/index.css';
+import "rc-drawer/assets/index.css";
 
-import {
-    BrowserRouter as Router,
-    Link,
-    Route,
-    Switch,
-  } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 class MobileDrawer extends Component {
-    render(){
-        return(
-            <Drawer style={{top:'20px'}}>
-                  <Menu
-                    
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
-                    mode="inline"
-                  >
-                    <Menu.Item key="QMR">
-                    <Link to='/' style={{ textDecoration: 'none' }}>
-                    QMR
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item key="analises">
-                    <Link to='/analises' style={{ textDecoration: 'none' }}>
-                    Análises
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item key="sobre">
-                    <Link to='/sobre' style={{ textDecoration: 'none' }}>
-                    Sobre
-                    </Link>
-                    </Menu.Item>
-                    <Menu.Item key="contato">
-                    <Link to='/contato' style={{ textDecoration: 'none' }}>
-                    Contato
-                    </Link>
-                    </Menu.Item>
-                  </Menu>
-                 </Drawer>
-        );
-    }
+  render() {
+    return (
+      <Drawer style={{ top: "20px" }}>
+        <Menu
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
+        >
+          <Menu.Item key="QMR">
+            <Link to="/qmrepresenta-reactjs" style={{ textDecoration: "none" }}>
+              QMR
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="analises">
+            <Link
+              to="/qmrepresenta-reactjs/analises"
+              style={{ textDecoration: "none" }}
+            >
+              Análises
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="sobre">
+            <Link
+              to="/qmrepresenta-reactjs/sobre"
+              style={{ textDecoration: "none" }}
+            >
+              Sobre
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="contato">
+            <Link
+              to="/qmrepresenta-reactjs/contato"
+              style={{ textDecoration: "none" }}
+            >
+              Contato
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Drawer>
+    );
+  }
 }
 
 export default MobileDrawer;
