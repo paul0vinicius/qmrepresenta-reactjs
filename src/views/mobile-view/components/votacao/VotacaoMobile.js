db236d4e29c7144993dc3531c352dffe526af257
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-07-27 15:46:28 -0300

    Ajusta cores de votação

--- src/views/mobile-view/components/votacao/VotacaoMobile.js
+++ src/views/mobile-view/components/votacao/VotacaoMobile.js
@@ -1,42 +1,54 @@
-import React, { Component } from 'react';
-import Votacao from '../../../desktop-view/components/votacao/Votacao.js';
-import Slider from 'react-slick';
-import ControlledExpansionPanelsMobile from './ExpansionPanelMobile.js';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Grid from 'material-ui/Grid';
-import Button from 'material-ui/Button';
-import Swipeable from 'react-swipeable';
-import { Collapse } from 'antd';
+import React, { Component } from "react";
+import Votacao from "../../../desktop-view/components/votacao/Votacao.js";
+import Slider from "react-slick";
+import ControlledExpansionPanelsMobile from "./ExpansionPanelMobile.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Grid from "material-ui/Grid";
+import Button from "material-ui/Button";
+import Swipeable from "react-swipeable";
+import { Collapse } from "antd";
 const Panel = Collapse.Panel;
 
+const COLOR_SIM = "rgba(29, 107, 145, 0.4)";
+const COLOR_NAO = "rgba(81, 38, 156, 0.4)";
+const COLOR_NAO_SEI = "rgba(219, 219, 219, 0.4)";
+
 class VotacaoMobile extends Votacao {
-  
-  colorPicker(){
+  colorPicker() {
     switch (this.props.value) {
       case 1:
-        return "#1D6B91";
+        return COLOR_SIM;
       case -1:
-        return "#51269C";
-      default: return "#DBDBDB";
+        return COLOR_NAO;
+      default:
+        return COLOR_NAO_SEI;
     }
   }
 
-  render(){
+  render() {
+    var painel = (
+      <ControlledExpansionPanelsMobile
+        pergunta={this.props.pergunta}
+        valorVoto={this.state.value}
+        descricao={this.props.descricao}
+      />
+    );
 
-    var painel = <ControlledExpansionPanelsMobile pergunta = {this.props.pergunta}
-                                                  valorVoto = {this.state.value}
-                                                  descricao = {this.props.descricao}
-        />;
+    var newPainel = (
+      <Collapse accordion>
+        <Panel
+          header={this.props.pergunta}
+          key={this.props.key}
+          showArrow={false}
+          style={{ backgroundColor: this.colorPicker() }}
+        >
+          <p>{this.props.descricao}</p>
+        </Panel>
+      </Collapse>
+    );
 
-    var newPainel = <Collapse accordion>
-                      <Panel header={this.props.pergunta} key={this.props.key} showArrow={false} style={{backgroundColor:this.colorPicker()}}>
-                        <p>{this.props.descricao}</p>
-                      </Panel>
-                    </Collapse>;
-        
-
-    return(
+    return (
       <div>
         <Swipeable
           onSwipingLeft={this.votaNao.bind(this)}
@@ -50,32 +62,40 @@ class VotacaoMobile extends Votacao {
     );
   }
 
-  votaSim(){
-    console.log('votou');
+  votaSim() {
+    console.log("votou");
     var state = this.state.value;
 
     switch (state) {
-      case 0: super.votaSim();
+      case 0:
+        super.votaSim();
         break;
-      case 1: super.votaSim();
+      case 1:
+        super.votaSim();
         break;
-      case -1: super.votaNaoSei();
+      case -1:
+        super.votaNaoSei();
         break;
-      default: break;
+      default:
+        break;
     }
   }
 
-  votaNao(){
+  votaNao() {
     var state = this.state.value;
 
     switch (state) {
-      case 0: super.votaNao();
+      case 0:
+        super.votaNao();
         break;
-      case 1: super.votaNaoSei();
+      case 1:
+        super.votaNaoSei();
         break;
-      case -1: super.votaNao();
+      case -1:
+        super.votaNao();
         break;
-      default: break;
+      default:
+        break;
     }
   }
 }
